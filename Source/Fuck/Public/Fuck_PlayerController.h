f8f48280d49eee793d121e4935aa26448fe17a7d
jcheetham3906 <128601268+jcheetham3906@users.noreply.github.com>
2024-05-07 09:09:19 +0100

    Start

---
+++ Source/Fuck/Public/Fuck_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Fuck_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUCK_API AFuck_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
